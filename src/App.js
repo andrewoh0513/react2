caf59b00fa74bb2852b316d8b3b6518e470c70e7
andrewoh0513 <143766112+andrewoh0513@users.noreply.github.com>
2024-04-07 02:32:57 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './circle.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Make react apps
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
